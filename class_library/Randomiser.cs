722bd539d61139beb0c7cd8b82c1d10b5b99a0b7
Tomislav0 <tomislav.kovacevic01@gmail.com>
2020-10-19 15:08:37 +0200

    Dodana zadaca

---
+++ class_library/Randomiser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace class_library
+{
+    public static class Randomiser
+    {
+        public static double GetRandomScore()
+        {
+            Random rand = new Random();
+            return rand.NextDouble() * 10;
+        }
+    }
+}
