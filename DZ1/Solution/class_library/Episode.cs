9e34ab65d7843389ca9ffe24f3c2ad0f425f61ce
Tomislav0 <72692790+Tomislav0@users.noreply.github.com>
2020-10-19 16:12:34 +0200

    Add files via upload

---
+++ DZ1/Solution/class_library/Episode.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace class_library
+{
+    
+    public class Episode
+    {
+        int views;
+        double score;
+        double maxscore;
+        
+        public Episode(int views,double score,double maxscore)
+        {
+            this.views = views;
+            this.score = score;
+            this.maxscore = maxscore;
+        }
+        public Episode()
+        {
+            views = 0;
+            score = 0;
+            maxscore = 0;
+        }
+        
+        public void AddView(double score)
+        {
+            views++;
+            this.score += score;
+            if (maxscore < score)
+            {
+                maxscore = score;
+            }
+        }
+        public double GetMaxScore()
+        {
+            return maxscore;
+        }
+        public double GetAverageScore()
+        {
+            return score / views;
+        }
+        public int GetViewerCount()
+        {
+            return views;
+        }
+    }
+}
